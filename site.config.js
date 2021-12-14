964510372b0fc02ac795d897aa8786686e566805
Marco Giraldo <marco.giraldo@unox.com>
2021-12-14 13:12:45 +0100

    Updates basic config.

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a8c84d65a5f941018d4bb8f38e2430fb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: null, // 'Transitive Bullshit',
+  domain: null, // 'transitivebullsh.it',
+  author: null, // 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: null, // 'Example site description',
+  socialImageTitle: null, // 'Transitive Bullshit',
+  socialImageSubtitle: null, // 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: null, // 'transitive_bs',
+  github: null, // 'transitive-bullshit',
+  linkedin: null, // 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
